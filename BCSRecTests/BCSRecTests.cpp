d689b0ad7bb85d7ed2ec86668871ce70799b133f
Pratyasa Roy <proy8892@conestogac.on.ca>
2026-02-21 23:39:26 -0500

    Add unit tests for setLength and setWidth

--- BCSRecTests/BCSRecTests.cpp
+++ BCSRecTests/BCSRecTests.cpp
@@ -29,6 +29,46 @@ namespace BCSRecTests
 
 			Assert::AreEqual(20, result);
 		}
+		TEST_METHOD(TestSetLength_ValidTypical_UpdatesLength)
+		{
+			int length = 10;
+			setLength(50, &length);
+			Assert::AreEqual(50, length);
+		}
+
+		TEST_METHOD(TestSetLength_InvalidNegative_DoesNotChangeLength)
+		{
+			int length = 10;
+			setLength(-1, &length);
+			Assert::AreEqual(10, length);
+		}
+
+		TEST_METHOD(TestSetLength_UpperBoundary_99Valid_UpdatesLength)
+		{
+			int length = 10;
+			setLength(99, &length);
+			Assert::AreEqual(99, length);
+		}
+		TEST_METHOD(TestSetWidth_ValidTypical_UpdatesWidth)
+		{
+			int width = 10;
+			setWidth(50, &width);
+			Assert::AreEqual(50, width);
+		}
+
+		TEST_METHOD(TestSetWidth_InvalidZero_DoesNotChangeWidth)
+		{
+			int width = 10;
+			setWidth(0, &width);
+			Assert::AreEqual(10, width);
+		}
+
+		TEST_METHOD(TestSetWidth_UpperBoundary_100Valid_UpdatesWidth)
+		{
+			int width = 10;
+			setWidth(100, &width);
+			Assert::AreEqual(100, width);
+		}
 		
 	};
 }
